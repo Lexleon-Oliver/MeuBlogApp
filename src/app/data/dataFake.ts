dfbb1dd49f547f79cff1d2f5de471599d35a166b
Lexleon-Oliver <lexleonmg@gmail.com>
2023-11-07 17:18:07 -0300

    :books: docs: Atualização do README

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,155 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "EVENTO DE RETORNO DO HOMEM DE FERRO",
+    "description": `
+
+    Você está ausente do Concurso há algum tempo e quer conferir todas as novas adições incríveis que foram feitas ao Jogo, mas precisa de uma ajudinha para se atualizar? O Homem de Ferro protege você!
+
+    O Homem de Ferro fez um grande retorno no Torneio e agora quer ajudar você a fazer o mesmo! Junte-se a ele neste evento que está disponível exclusivamente para jogadores que retornaram e que perderam algumas das grandes novidades e eventos do Torneio.
+
+    Este evento é para jogadores que retornam e está disponível apenas para jogadores que não fizeram login desde 16 de agosto de 2023 às 17h UTC ou antes. Ele estará disponível apenas de 16 de outubro às 10h PT até 6 de novembro de 2023 às 10h PT!
+    Obtenha o Homem de Ferro e seu kit atualizado gratuitamente
+
+    O Homem de Ferro acaba de receber uma grande atualização, completa com um novo conjunto de habilidades, animações e um visual totalmente novo. Os invocadores adoraram essa reformulação, e você terá a chance de experimentar o novo Homem de Ferro!
+
+    Os jogadores que retornarem e participarem do evento Iron Man's Comeback receberão um Iron Man ao fazer login. Experimente-o e experimente sua atualização por si mesmo!
+    Acompanhe o que você perdeu
+
+    Durante o último ano, houve grandes adições e melhorias no The Contest, e nunca houve melhor momento para os jogadores voltarem e experimentarem tudo!
+
+    Em Iron Man's Comeback, você receberá um conjunto de objetivos, cada um apontando para conteúdos e recursos novos e atualizados. Esses objetivos são determinados pelo seu nível de progressão (Uncollected, Thronebreaker, etc.). Concluir esses objetivos irá recompensá-lo com ótimos prêmios para ajudá-lo a fortalecer seus campeões e construir seu elenco!
+    O que eu perdi?
+
+    Dependendo de quando você jogou Marvel Contest of Champions pela última vez, você pode ter perdido algumas adições e melhorias significativas!
+    Campos de batalha
+
+    Introduzido há 1 ano, Battlegrounds é a melhor experiência MCoC 1v1! Construa seu Baralho de Campeões e enfrente seu oponente! Você selecionará seus atacantes e defensores em tempo real com seu oponente e batalhará em ação PvP assíncrona!
+    Ascensão
+
+    Ascensão é uma mecânica totalmente nova no Torneio que permite que você fortaleça seus Campeões de 4, 5 e 6 Estrelas além dos limites atuais!
+
+    Isso ajuda os Recrutadores a prolongar a vida de sua Lista à medida que passam de uma raridade para outra, mas também oferece mais oportunidades de voltar e usar alguns antigos favoritos que podem parecer um pouco ultrapassados ​​agora.
+    Relíquias
+
+    As relíquias permitem que você customize seus campeões e adicione um parceiro de tag team às suas partidas! Anexe relíquias aos seus campeões para aumentar suas habilidades ou aumentar suas estatísticas e, em seguida, solte seu atacante em seu oponente quando for o momento certo!
+    Atualização de Incursões
+
+    Desde que introduzimos as Incursões, os jogadores pediram a possibilidade de jogá-las Solo e, no início deste ano, tornamos isso possível! Você não precisa mais encontrar um parceiro para jogar Incursões! Você ainda pode jogar com um amigo, mas isso não é mais obrigatório.
+
+    Fizemos grandes mudanças nos mapas, reduzimos os tempos de recarga dos Campeões usados ​​nas Incursões e tornamos os Bônus mais poderosos e que duram muito mais tempo!
+
+    Confira a loja Incursions para mais opções de cristais do que nunca!
+    Retrabalho dos Atos 4 e 5
+
+    Os Atos 4 e 5 receberam algumas atualizações importantes! Reduzimos o número de lutas, caminhos e complexidade! Você encontrará menos obstáculos e combinações de nós intimidantes, levando a uma rampa de dificuldade muito mais suave!
+    Ato 6 e 7 Remoção do Portão de Raridade
+
+    Os portões de raridade foram removidos dos Atos 6 e 7! Agora você pode trazer seus campeões de 4 estrelas recém-ascendidos para ajudá-lo a vencer os desafios que o Battlerealm apresenta em seu caminho!
+    Que recompensas aguardam meu retorno?
+
+    As recompensas por completar os objetivos do Retorno do Homem de Ferro são baseadas no seu Nível de Progressão, mas você pode esperar receber recompensas raras e poderosas, incluindo Catalisadores, para ajudá-lo a classificar seus Campeões. Bem como joias do despertar para despertar suas habilidades únicas e fragmentos de cristal para ajudá-lo a construir sua lista com novos campeões!
+
+    Vejo vocês no concurso, invocadores!
+      `,
+    "photoCover":"https://playcontestofchampions.com/wp-content/uploads/2023/04/hero-iron-man-comeback.jpg"
+  },
+  {
+    "id":"2",
+    "title": "NOTAS DE VERSÃO V41.2",
+    "description": `Missão de Evento – Lua de Sangue
+
+    Algo está errado no pântano do Battlerealm! Junte-se aos veteranos de Midnight Sons, Blade e Elsa Bloodstone , enquanto eles investigam desaparecimentos ligados à paisagem pantanosa. Encontre-se com WEREWOLF BY NIGHT e um elenco de curiosidades monstruosas enquanto os mistérios dos pântanos se revelam e as maquinações diabólicas do gênio caluniado MORBIUS começam a entrar em foco. Será que esta equipe conseguirá expor a verdade e acabar com o caos que se manifesta e resgatar aqueles que foram injustiçados pelo Vampiro Vivo?
+    Meu pequeno simbionte – Sugar Crash
+
+    O Colecionador está farto de ver o Invocador intrometido roubando seu magnífico cofre de tesouros e recursos, então eles trancaram todos os seus Campeões do lado de fora. Ou assim eles pensaram. Felizmente, Reed Richards abriu este cofre com a ajuda de um membro raro da equipe do Invocador: O SIMBIÓTE CONVOCADO!
+
+    Faça check-in semanalmente para ter tempo narrativo bônus com sua criatura favorita feita de presas e gosma enquanto você reúne riquezas, recursos e, claro, DOCES!
+    Vazio de Knull
+
+    Espalhou-se a notícia de que você está tentando fortalecer um amigo simbionte seu, e Knull simpatiza com esse objetivo. No entanto, ele não está disposto a dar poder àqueles que não o conquistaram! Viaje até o Vazio para obter recursos raros que permitirão que você fortaleça seu Simbionte Convocado enquanto enfrenta outros desafios divertidos que este mês tem a oferecer!
+
+    Ato 8 Capítulo 3 - Iluminação
+
+    A estação de Bahamet foi fechada, mas a luta quase foi demais para Carina e sua companheira de campeões. A luta foi transmitida para todo o Campo de Batalha, e todas as forças do mal do Torneio parecem estar se aproximando. O Conselho está chamando Carina e a tripulação de volta para um interrogatório, mas o ímpeto desta vitória deve ser aproveitado sem demora. Há uma escuridão crescente lá fora e ela precisa ser afastada por qualquer luz de esperança que Carina consiga manter acesa. Será isto uma faísca de revolução ou a chama que incendiará todo o Concurso? Descubra no Ato 8 – Capítulo 3: ILUMINAÇÃO!
+
+    Atualizações de Campeões
+
+    A atualização de equilíbrio da Viv Vision chega no Concurso! Confira nosso blog para mais detalhes. `,
+    "photoCover":"https://playcontestofchampions.com/wp-content/uploads/2023/04/release-notes-41-2.jpg"
+  },
+  {
+    "id":"3",
+    "title": "O QUE É ASCENSÃO?",
+    "description": `A Ascensão chegará ao Torneio no dia 6 de setembro, para seus campeões de 4, 5 e 6 estrelas! Colete Pó Primordial para aumentar o poder dos seus Campeões favoritos!
+    O que é Ascensão?
+
+    A ascensão é uma forma de tornar mais fortes os seus campeões de 4, 5 e 6 estrelas existentes. Quando um Campeão ascende, ele ganha um aumento em seu Ataque e HP. Ascender um Campeão não afetará o Prestígio desse Campeão, porém, assim como as Maestrias, aumentará seu IP.
+    Raridade 	Aumento de Ataque 	Aumento da saúde
+    4 estrelas 	30% 	30%
+    5 estrelas 	20% 	20%
+    6 estrelas 	22% 	22%
+
+    A Ascensão permitirá a sobreposição de poder entre as raridades dos Campeões –
+
+        Campeões Ascensionados de 4 Estrelas no máximo irão se sobrepor a Campeões de Rank 4 de 5 Estrelas que não são Ascensionados, perto de seu nível máximo
+        Campeões Ascensionados de 5 Estrelas no máximo irão se sobrepor a Campeões de Rank 3 de 6 Estrelas que não são Ascensionados, perto de seu nível máximo
+        Os Campeões Ascensionados de 6 Estrelas no máximo se sobreporão aos Campeões de Rank 3 de 7 Estrelas logo após o início da classificação
+
+    Como faço para ascender meus campeões?
+
+    Para ascender seus campeões, você precisará coletar Pó Primordial! Poeira Primordial de Nível 1 será para Campeões Ascendentes de 4 e 5 Estrelas, e Poeira Primordial de Nível 2 será para Campeões Ascendentes de 6 Estrelas.
+
+    Você poderá manter 200 Pó Primordial de Nível 1 (250 com o Sigilo do Invocador) e 400 Pó Primordial de Nível 2 (500 com o Sigilo do Invocador) em seu inventário. Qualquer Pó Primordial extra será transferido para o seu Estoque!
+
+    Um campeão pode ser ascendido em qualquer posto e em qualquer nível.
+
+    Semelhante a Nivelar e Classificar um Campeão, Ascender um Campeão estará disponível na mesma tela e custará Ouro e Pó Primordial.
+
+    Depois que um Campeão ascende, isso não pode ser desfeito.
+
+    Campeões de 1, 2, 3 estrelas e 7 estrelas não estão disponíveis para Ascensão. `,
+    "photoCover":"https://playcontestofchampions.com/wp-content/uploads/2023/04/hero-ascension-before-after.jpg"
+  },
+  {
+    "id":"4",
+    "title": "NOTAS DE VERSÃO V41.1",
+    "description": `Missão de Evento – Ecos na Eternidade
+
+    O Gladiador e o Homem de Ferro devem se unir contra os aparentemente imparáveis ​​Mestres do Mal! O Barão Zemo está de volta e, desta vez, aperfeiçoou sua ISO-Sphere caseira e está de olho na conquista do Battlerealm. Esses heróis conseguirão derrotar Zemo sem sucumbir ao poder da ISO-Sphere?
+    novo
+    O Campeão Escolha do Invocador chegou!
+
+    Depois de provar seu valor como um poderoso guerreiro, Kallark de Strontia assumiu o manto de Gladiador e se tornou o renomado líder da Guarda Imperial Shi'ar. Apesar do poder quase ilimitado do Strontian, ele é um dos últimos de sua espécie e honra a memória de seu povo servindo como guarda-costas pessoal do Majestor e Majestrix Shi'ar. Ele é um dos maiores heróis da galáxia, desde que você esteja alinhado com os objetivos do Império Shi'ar.
+    Atualizada
+    Homem de Ferro recebe uma grande atualização!
+
+    Este mês, em vez de um segundo Campeão, reservamos um tempo para dar ao Iron Man , um Campeão que estava disponível quando lançamos o jogo em 2014, uma reformulação total! Ele tem um visual totalmente novo, animações totalmente novas e um conjunto de habilidades completamente reformulado! E o melhor de tudo, ele já está na sua lista! O novo e melhorado Homem de Ferro substituirá o Homem de Ferro Original no jogo quando você atualizar para a v41.1.
+    novo
+    Missão de Evento – Manopla Paragon
+
+    Desafie-se enfrentando todos os chefes da missão do evento ECHOES IN ETERNITY, um após o outro. Com um elenco reduzido para aumentar a complexidade, esta nova dificuldade testará suas habilidades e sua determinação!
+    novo
+    Manopla do Gladiador
+
+    No planeta Strontia, jovens homens e mulheres strontianos treinariam e competiriam pelo título de Gladiador. Agora o Grande Mestre passou por uma das provações mais difíceis e criou a sua própria para testar a determinação dos maiores Campeões do Campo de Batalha. Eis: A Manopla do Gladiador! Lute por todos os 9 níveis deste cansativo Desafio e torne-se parte de um Império!
+    novo
+    Ascensão
+
+    O tão esperado recurso finalmente chegou! Adicionamos a capacidade de ascender seus campeões de 4 a 6 estrelas, concedendo-lhes maior ataque e saúde além dos limites anteriores por meio do uso de um novo recurso de Pó Primordial.
+    Atualizada
+    É Pool Party nas Incursões!
+
+    Deadpool está assumindo o controle das Incursões! Ele está trazendo consigo uma série de adições permanentes do evento Legendary Incursions do mês passado e atualizações para a Loja de Incursões. Fique atento para mais detalhes!
+    novo
+    Evento de alistamento do Cap
+
+    O CAP QUER VOCÊ! Junte-se a uma Aliança e participe do Evento de Alistamento do Capitão; um evento de um mês focado em Guerra de Aliança, Missão de Aliança, Campos de Batalha e Incursões. Presenteie Cristais de Alistamento, ganhe recompensas nos eventos Solo de Alistamento e Aliança e ganhe ainda mais em Objetivos Solo de Alistamento com Limite Especial.
+    Atualizada
+    Atualizações de Campeões
+
+    Além da atualização do Homem de Ferro , também fizemos atualizações de equilíbrio e grandes correções de bugs para Homem-Formiga (Futuro) e Cassie Lang !
+    `,
+    "photoCover":"https://playcontestofchampions.com/wp-content/uploads/2023/04/release-notes-41-1.jpg"
+  },
+]
